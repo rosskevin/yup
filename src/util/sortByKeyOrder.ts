99b7eaa335943b3dcf4a5f93ce0b95a7a5d48e77
Kevin Ross <kevin.ross@alienfast.com>
2019-01-03 00:00:43 -0600

    ts

--- src/util/sortByKeyOrder.ts
+++ src/util/sortByKeyOrder.ts
@@ -1,18 +1,21 @@
-function findIndex(arr, err) {
+import ValidationError from '../ValidationError' // this may or may not be right - based on the err name below
+
+function findIndex(arr: string[], err: ValidationError) {
   let idx = Infinity
   arr.some((key, ii) => {
     if (err.path.indexOf(key) !== -1) {
       idx = ii
       return true
     }
+    return false
   })
 
   return idx
 }
 
-export default function sortByKeyOrder(fields) {
-  let keys = Object.keys(fields)
-  return (a, b) => {
+export default function sortByKeyOrder(fields: object) {
+  const keys = Object.keys(fields)
+  return (a: ValidationError, b: ValidationError) => {
     return findIndex(keys, a) - findIndex(keys, b)
   }
 }
