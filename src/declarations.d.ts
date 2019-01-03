bff083880e40c8877d7be003149e4fb9e3acaa7c
Kevin Ross <kevin.ross@alienfast.com>
2019-01-02 23:51:09 -0600

    ts sortFields

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,1 +1,2 @@
 declare module 'property-expr'
+declare module 'toposort'
--- src/util/sortFields.ts
+++ src/util/sortFields.ts
@@ -1,30 +1,43 @@
 import has from 'lodash/has'
-import toposort from 'toposort'
 import { split } from 'property-expr'
+import toposort from 'toposort'
 import Ref from '../Reference'
 import isSchema from './isSchema'
 
-export default function sortFields(fields, excludes = []) {
-  var edges = [],
-    nodes = []
+export default function sortFields(fields: any, excludes: any[] = []) {
+  const edges: any[] = []
+  const nodes: any[] = []
 
-  function addNode(depPath, key) {
-    var node = split(depPath)[0]
+  function addNode(depPath: string, key: string) {
+    const node = split(depPath)[0]
 
-    if (!~nodes.indexOf(node)) nodes.push(node)
+    // tslint:disable-next-line:no-bitwise
+    if (!~nodes.indexOf(node)) {
+      nodes.push(node)
+    }
 
-    if (!~excludes.indexOf(`${key}-${node}`)) edges.push([key, node])
+    // tslint:disable-next-line:no-bitwise
+    if (!~excludes.indexOf(`${key}-${node}`)) {
+      edges.push([key, node])
+    }
   }
 
-  for (var key in fields)
+  for (const key in fields) {
     if (has(fields, key)) {
-      let value = fields[key]
+      const value = fields[key]
 
-      if (!~nodes.indexOf(key)) nodes.push(key)
+      // tslint:disable-next-line:no-bitwise
+      if (!~nodes.indexOf(key)) {
+        nodes.push(key)
+      }
 
-      if (Ref.isRef(value) && !value.isContext) addNode(value.path, key)
-      else if (isSchema(value) && value._deps) value._deps.forEach(path => addNode(path, key))
+      if (Ref.isRef(value) && !value.isContext) {
+        addNode(value.path, key)
+      } else if (isSchema(value) && value._deps) {
+        value._deps.forEach((path: string) => addNode(path, key))
+      }
     }
+  }
 
   return toposort.array(nodes, edges).reverse()
 }
