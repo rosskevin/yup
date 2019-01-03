37b84be287e20df064b6bf9e6e57ad54de44ec71
Kevin Ross <kevin.ross@alienfast.com>
2019-01-02 23:18:06 -0600

    convert to ts

--- src/util/printValue.ts
+++ src/util/printValue.ts
@@ -1,3 +1,5 @@
+// tslint:disable:ban-types
+
 const toString = Object.prototype.toString
 const errorToString = Error.prototype.toString
 const regExpToString = RegExp.prototype.toString
@@ -5,39 +7,63 @@ const symbolToString = typeof Symbol !== 'undefined' ? Symbol.prototype.toString
 
 const SYMBOL_REGEXP = /^Symbol\((.*)\)(.*)$/
 
-function printNumber(val) {
-  if (val != +val) return 'NaN'
+function printNumber(val: number): string {
+  if (val !== +val) {
+    return 'NaN'
+  }
   const isNegativeZero = val === 0 && 1 / val < 0
   return isNegativeZero ? '-0' : '' + val
 }
 
-function printSimpleValue(val, quoteStrings = false) {
-  if (val == null || val === true || val === false) return '' + val
+export type Value = number | string | Function | null | boolean | Date
+function printSimpleValue(val: Value, quoteStrings = false) {
+  if (val === null || val === true || val === false) {
+    return '' + val
+  }
 
   const typeOf = typeof val
-  if (typeOf === 'number') return printNumber(val)
-  if (typeOf === 'string') return quoteStrings ? `"${val}"` : val
-  if (typeOf === 'function') return '[Function ' + (val.name || 'anonymous') + ']'
-  if (typeOf === 'symbol') return symbolToString.call(val).replace(SYMBOL_REGEXP, 'Symbol($1)')
+  if (typeOf === 'number') {
+    return printNumber(val as number)
+  }
+  if (typeOf === 'string') {
+    return quoteStrings ? `"${val}"` : val
+  }
+  if (typeOf === 'function') {
+    return '[Function ' + ((val as Function).name || 'anonymous') + ']'
+  }
+  if (typeOf === 'symbol') {
+    return symbolToString.call(val).replace(SYMBOL_REGEXP, 'Symbol($1)')
+  }
 
   const tag = toString.call(val).slice(8, -1)
-  if (tag === 'Date') return isNaN(val.getTime()) ? '' + val : val.toISOString(val)
-  if (tag === 'Error' || val instanceof Error) return '[' + errorToString.call(val) + ']'
-  if (tag === 'RegExp') return regExpToString.call(val)
+  if (tag === 'Date') {
+    return isNaN((val as Date).getTime()) ? '' + val : (val as Date).toISOString()
+  }
+  if (tag === 'Error' || val instanceof Error) {
+    return '[' + errorToString.call(val) + ']'
+  }
+  if (tag === 'RegExp') {
+    return regExpToString.call(val)
+  }
 
   return null
 }
 
-export default function printValue(value, quoteStrings) {
-  let result = printSimpleValue(value, quoteStrings)
-  if (result !== null) return result
+export default function printValue(value: Value, quoteStrings: boolean) {
+  const result = printSimpleValue(value, quoteStrings)
+  if (result !== null) {
+    return result
+  }
 
   return JSON.stringify(
     value,
-    function(key, value) {
-      let result = printSimpleValue(this[key], quoteStrings)
-      if (result !== null) return result
-      return value
+    (k, v) => {
+      // const psv = printSimpleValue(this[key], quoteStrings) // old
+      const psv = printSimpleValue(v, quoteStrings)
+      if (psv !== null) {
+        return psv
+      }
+      return v
     },
     2,
   )
