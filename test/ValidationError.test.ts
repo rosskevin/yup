294220cde598396dd603a690bb65352f60ff96f9
Kevin Ross <kevin.ross@alienfast.com>
2019-01-04 20:06:33 -0600

    test ValidationError prior to conversion

---
+++ test/ValidationError.test.ts
@@ -0,0 +1,16 @@
+import ValidationError from '../src/ValidationError'
+
+describe('ValidationError', () => {
+  const ve = new ValidationError('a message', 'a value', 'a.path', 'aType')
+  it('name', () => {
+    expect(ve.name).toStrictEqual('ValidationError')
+  })
+
+  it('instanceof', () => {
+    expect(ve instanceof ValidationError).toStrictEqual(true)
+  })
+
+  it('typeof', () => {
+    expect(typeof ve).toStrictEqual('object')
+  })
+})
--- test/ValidationError.test.tsx
+++
@@ -1,7 +0,0 @@
-import ValidationError from '../src/ValidationError'
-
-describe('ValidationError', () => {
-  it('this is a placeholder', () => {
-    expect('foo').toStrictEqual('foo')
-  })
-})
