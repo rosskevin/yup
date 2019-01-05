66a098e14fd3d95b3da2d97ff5284acfb0d3442f
Kevin Ross <kevin.ross@alienfast.com>
2019-01-04 20:12:15 -0600

    ValidationError to class

--- src/ValidationError.js
+++ src/ValidationError.js
@@ -1,30 +1,30 @@
-export default function ValidationError(errors, value, field, type) {
-  this.name = 'ValidationError'
-  this.value = value
-  this.path = field
-  this.type = type
-  this.errors = []
-  this.inner = []
+export default class ValidationError extends Error {
+  constructor(errors, value, field, type) {
+    super()
+    this.name = 'ValidationError'
+    this.value = value
+    this.path = field
+    this.type = type
+    this.errors = []
+    this.inner = []
 
-  if (errors) {
-    ;[].concat(errors).forEach(err => {
-      this.errors = this.errors.concat(err.errors || err)
+    if (errors) {
+      ;[].concat(errors).forEach(err => {
+        this.errors = this.errors.concat(err.errors || err)
 
-      if (err.inner) {
-        this.inner = this.inner.concat(err.inner.length ? err.inner : err)
-      }
-    })
-  }
+        if (err.inner) {
+          this.inner = this.inner.concat(err.inner.length ? err.inner : err)
+        }
+      })
+    }
 
-  this.message = this.errors.length > 1 ? `${this.errors.length} errors occurred` : this.errors[0]
+    this.message = this.errors.length > 1 ? `${this.errors.length} errors occurred` : this.errors[0]
 
-  if (Error.captureStackTrace) {
-    Error.captureStackTrace(this, ValidationError)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ValidationError)
+    }
   }
 }
-
-ValidationError.prototype = Object.create(Error.prototype)
-ValidationError.prototype.constructor = ValidationError
 
 ValidationError.isInstance = function(err) {
   return err && err.name === 'ValidationError'
