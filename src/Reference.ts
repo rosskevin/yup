d78d24a6e318e9b3be12b5b6a3127f729e499ac6
Kevin Ross <kevin.ross@alienfast.com>
2019-01-04 23:47:44 -0600

    update Reference types

--- src/Reference.ts
+++ src/Reference.ts
@@ -14,8 +14,30 @@ export interface Options {
   contextPrefix?: string
 }
 
+// It is tempting to declare `Ref` very simply, but there are problems with these approaches:
+//
+// * `type Ref = Record<string, any>;` - This is essentially how it was originally declared, but
+//    just about any object satisfies this contract, which makes the type declaration too loose to
+//    be useful.
+//
+// * `type Ref = object;` - This is a variation on the previous bullet in that it is too loose.
+//
+// * `class Ref {}` - This is yet another variation that is too loose.
+//
+// * `type Ref = void;` - This works and the emitted JavaScript is just fine, but it results in some
+//    confusing IntelliSense, e.g it looks like the `ref()` returns `void`, which is not the case.
+//
+// The solution is twofold. 1.) Declare it as a class with a private constructor to prevent it from
+// being instantiated by anything but the `ref()` factory function, and; 2.) declare a private
+// readonly property (that yup actually places on the prototype) to force it to be structurally
+// incompatible with any other object type.
+
+/**
+ * `Ref` is an opaque type that is internal to yup. Creating a `Ref` instance is accomplished via the `ref()` factory
+ * function.
+ */
 export default class Reference {
-  public static isRef(value: any) {
+  public static isRef(value: any): value is Reference {
     return !!(value && (value.__isYupRef || value instanceof Reference))
   }
 
@@ -26,7 +48,7 @@ export default class Reference {
   public isSelf: boolean
   public path: string
   // tslint:disable-next-line:variable-name
-  public __isYupRef: boolean = true
+  public readonly __isYupRef: boolean = true
   private pathGetter: any
 
   constructor(keyArg: string | Function, mapFn: MapFn, options: Options = {}) {
