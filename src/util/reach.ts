29cfa48ed79f9d8c1683fb3e06484d569d543644
Kevin Ross <kevin.ross@alienfast.com>
2019-01-30 20:47:30 -0600

    fix getIn import

--- src/util/reach.ts
+++ src/util/reach.ts
@@ -1,5 +1,5 @@
 import { MixedSchema } from '../MixedSchema'
-import getIn from './getIn'
+import { getIn } from './getIn'
 
 export function reach<S extends MixedSchema>(schema: S, path: string, value?: any, context?: any) {
   return getIn(schema, path, value, context).schema
