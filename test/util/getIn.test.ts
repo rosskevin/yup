01c9e651ddd68922168f2efff3b4895f26131f62
Kevin Ross <kevin.ross@alienfast.com>
2019-01-30 20:12:19 -0600

    re-port test

--- test/util/getIn.test.ts
+++ test/util/getIn.test.ts
@@ -2,21 +2,21 @@ import { array, number, object } from 'yup'
 import getIn from '../../src/util/getIn'
 
 describe('getIn', () => {
-  it('should getIn correctly', async () => {
+  it('should getIn correctly', () => {
     const num = number()
     const inst = object().shape({
       num: number().max(4),
 
       nested: object().shape({
-        arr: array().of(object().shape({ num })),
+        arr: array().of(object().shape({ 'num-1': num })),
       }),
     })
 
-    const value = { nested: { arr: [{}, { num: 2 }] } }
-    const { schema, parent, parentPath } = getIn(inst, 'nested.arr[1].num', value)
+    const value = { nested: { arr: [{}, { 'num-1': 2 }] } }
+    const { schema, parent, parentPath } = getIn(inst, 'nested.arr[1].num-1', value)
 
     expect(schema).toStrictEqual(num)
-    expect(parentPath).toStrictEqual('num')
-    expect(parent).toStrictEqual(value.nested.arr[1])
+    expect(parentPath).toStrictEqual('num-1')
+    expect(parent).toMatchObject(value.nested.arr[1])
   })
 })
